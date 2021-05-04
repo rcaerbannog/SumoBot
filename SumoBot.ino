f849fe9f9cd5e9d7de526a60088ab2148e1f4d56
anthonyferrante38 <83514263+anthonyferrante38@users.noreply.github.com>
2021-05-04 15:13:25 -0400

    Update SumoBot.ino

--- SumoBot.ino
+++ SumoBot.ino
@@ -4,7 +4,7 @@ int on_off_pin=0;
 int enable1_pin=1;
 int enable2_pin=2;
 int enable3_pin=3;
-int enable4_pin=4
+int enable4_pin=4;
 int uds_left_pin=5;
 int uds_right_pin=6;
 int uds_center_pin=7;
@@ -13,25 +13,25 @@ int ldr1_pin=A0;
 int ldr2_pin=A1;
 int dip1_pin=A4;
 int dip2_pin=A5;
-int LDR1state=0;
-int LDR2state=0;
+int ldr1_state=0;
+int ldr2_state=0;
 
 void setup() {
   lcd.begin(16,2);
   
-  pinMode(LDR1, INPUT);
-  pinMode(LDR2, INPUT);
+  pinMode(ldr1_pin, INPUT);
+  pinMode(ldr2_pin, INPUT);
 }
 
 void loop() {
-  LDR1state=analogRead(LDR1);
+  ldr1_state=analogRead(ldr1_pin);
   
   lcd.setCursor(12, 0);
-  lcd.print(LDR1state);
+  lcd.print(ldr1_state);
     
-  LDR2state=analogRead(LDR2);
+  ldr2_state=analogRead(ldr2_pin);
   
   lcd.setCursor(12, 1);
-  lcd.print(LDR2state);
+  lcd.print(ldr2_state);
 }
   
